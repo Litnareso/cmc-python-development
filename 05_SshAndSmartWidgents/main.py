2d91944864df8d3b51645ff0d282b5536f28db24
litnareso <litnareso@protonmail.com>
2021-04-04 20:11:47 +0300

    Implement drawing from text

--- 05_SshAndSmartWidgents/main.py
+++ 05_SshAndSmartWidgents/main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import re
 
 
 class Application(tk.Frame):
@@ -27,6 +28,8 @@ class Application(tk.Frame):
     def createText(self, parent):
         self.descr = tk.Text(parent)
         self.descr.grid(sticky="NSWE")
+        self.descr.bind('<Control-l>', self.updateConv)
+        self.descr.tag_configure("wrong_line", background="red")
 
     def createPaint(self, parent):
         self.canv = tk.Canvas(parent, takefocus=True)
@@ -80,12 +83,38 @@ class Application(tk.Frame):
         for id in self.canv.find_all():
             colorFill = self.canv.itemcget(id, "fill")
             colorOutline = self.canv.itemcget(id, "outline")
-            outline = self.canv.itemcget(id, "offset").replace(",", ".")
+            outline = self.canv.itemcget(id, "width").replace(",", ".")
             coords = self.canv.coords(id)
             self.descr.insert("end", "oval <" +
                               "{:.1f} {:.1f} {:.1f} {:.1f}> ".format(*coords) +
                               "{:.1f}".format(float(outline)) + " " +
                               colorOutline + " " + colorFill + "\n")
+
+    def updateConv(self, event):
+        text = self.descr.get("1.0", "end-1c")
+        refloat = r"([-+]?\d*\.\d+|\d+)"
+        recolor = r"([\da-zA-Z#]+)"
+        lines = text.split("\n")
+        self.canv.delete('all')
+        self.descr.tag_remove("wrong_line", 1.0, "end")
+        for num, line in enumerate(lines):
+            if len(line) == 0:
+                continue
+            res = re.fullmatch("oval <" + refloat + " " + refloat + " " +
+                               refloat + " " + refloat + "> " + refloat + " " +
+                               recolor + " " + recolor, line)
+            if res is None:
+                self.descr.tag_add("wrong_line", "%d.0" % (num + 1),
+                                   "%d.0" % (num + 2))
+                continue
+            coords = list(float(x) for x in res.groups()[:4])
+            paintStyle = {'width': float(res.group(5)), 'fill': res.group(7),
+                          'outline': res.group(6)}
+            try:
+                self.canv.create_oval(*coords, paintStyle)
+            except tk.TclError:
+                self.descr.tag_add("wrong_line", "%d.0" % (num + 1),
+                                   "%d.0" % (num + 2))
 
 
 def main():
